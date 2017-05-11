7145c47504fff53aec68e9d9c84339f19a65edd9
Matthias Weis <matthias1.weis@web.de>
2017-05-11 08:14:05 +0200

    Initialer Commit

---
+++ inc/main.h
@@ -0,0 +1,15 @@
+/*
+ * main.h
+ *
+ *  Created on: 10.05.2017
+ *      Author: matthias
+ */
+
+#ifndef MAIN_H_
+#define MAIN_H_
+
+#include "stm32l4xx_ll_bus.h"
+
+
+
+#endif /* MAIN_H_ */
---
+++ src/main.c
@@ -0,0 +1,10 @@
+/*
+ * main.c
+ *
+ *  Created on: 10.05.2017
+ *      Author: matthias
+ */
+
+int main(void){
+	;
+}
