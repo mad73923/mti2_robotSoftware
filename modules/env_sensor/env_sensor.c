bc135d614d9ebf171b69f9667fd65e996c9bb0dc
Felix <s67857@beuth-hochschule.de>
2017-05-17 09:33:26 +0200

    First files for environment Sensors created

---
+++ modules/env_sensor/env_sensor.c
@@ -0,0 +1,7 @@
+/*
+* env_sensor.c
+ *
+ *  Created on: 17.05.2017
+ *      Author: felix
+ */
+#include "env_sensor.h"
---
+++ modules/env_sensor/env_sensor.h
@@ -0,0 +1,15 @@
+/*
+ * env_sensor.h
+ *
+ *  Created on: 17.05.2017
+ *      Author: felix
+ */
+
+#ifndef ENV_SENSOR_ENV_SENSOR_H_
+#define ENV_SENSOR_ENV_SENSOR_H_
+
+
+
+
+
+#endif /* ENV_SENSOR_ENV_SENSOR_H_ */
