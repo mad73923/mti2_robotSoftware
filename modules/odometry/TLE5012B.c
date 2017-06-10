6f57fb2a30c3e7766de402493d5cf35222dcf8f4
Matthias Weis <matthias1.weis@web.de>
2017-06-11 01:36:01 +0200

    added writeRegister tweak

--- modules/odometry/TLE5012B.c
+++ modules/odometry/TLE5012B.c
@@ -56,7 +56,7 @@ void sensor_writeRegister(TLE5012B_REG_t reg, uint16_t value, TLE5012B_ACT_t sid
 
 	CS_activateSide(side);
 
-	SPI_communicate_sync(&txBuffer[0], 2, 0, 0);
+	SPI_communicate_sync(&txBuffer[0], 2, 0, 1);
 
 	CS_resetAll();
 
