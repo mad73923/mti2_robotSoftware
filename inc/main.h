272d194fdd89eac009f96b23e41dcc4d2da26516
Felix <s67857@beuth-hochschule.de>
2017-06-25 12:58:05 +0200

    main.c/h added

--- inc/main.h
+++ inc/main.h
@@ -10,6 +10,9 @@
 
 #include "../modules/system/system.h"
 #include "../modules/debug/debug.h"
+//#include "../modules/motor/motor.h"
+#include "../modules/env_sensor/env_sensor.h"
+
 
 
 #endif /* MAIN_H_ */
