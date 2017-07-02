80796576cc3b7410997045827a8fcfc6e9dcddea
Felix <s67857@beuth-hochschule.de>
2017-07-02 13:17:03 +0200

    Return Value for setSpeed returns now negative values

--- modules/motor/motor.c
+++ modules/motor/motor.c
@@ -7,6 +7,9 @@
 #include "motor.h"
 
 
+//Private Variables
+char cMem=0;
+
 /**
   * @brief  With this function the GPIO for the direction for MotorLeft &
   * MotorRight will be configured as Output. Furthermore the Beep Output-Pin
@@ -35,16 +38,25 @@ int32_t motor_setSpeed(char cMotor, int32_t iSpeed){
 	//Check direction
 	if(iSpeed<0){
 		set_dir(cMotor,BACKWARD);
-		iSpeed*=-1;
+		cMem = 1;
 	}else{
 		set_dir(cMotor,FORWARD);
+		cMem = 0;
 	}
 
 	//Check Speed-Limit
 	if(iSpeed>LL_TIM_GetAutoReload(TIM4)){
-		return set_cc(cMotor, LL_TIM_GetAutoReload(TIM4));
+		if(cMem == 0){
+			return set_cc(cMotor, LL_TIM_GetAutoReload(TIM4));
+		}else{
+			return -(set_cc(cMotor,LL_TIM_GetAutoReload(TIM4)));
+		}
 	}else{
-		return set_cc(cMotor, iSpeed);
+		if(cMem == 0){
+			return set_cc(cMotor, iSpeed);
+		}else{
+			return -(set_cc(cMotor,(-1)*iSpeed));
+		}
 	}
 }
 
